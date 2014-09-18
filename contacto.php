813a0bfa001d8e29d6b0e01826f177f85207086f
Frander <frander_mejia@outlook.com>
2014-09-18 12:22:03 -0600

    commit final 99%, talvez haga pocos cambios pero nada drastico

---
+++ contacto.php
@@ -0,0 +1,43 @@
+<?php
+require 'libs/class.phpmailer.php';
+
+$mail = new PHPMailer();
+ 
+$mail->IsSMTP();
+$mail->Host     = "ssl://smtp.gmail.com";
+$mail->Port     = 465;
+$mail->SMTPAuth = true;
+ 
+$mail->Username = "frander.mejia15@gmail.com";
+$mail->Password = "Guaymitas12";
+
+$email = $_POST['email'];
+$nombre = $_POST['nombre'];
+ 
+$mail->From     = $email
+$mail->FromName = $nombre
+ 
+$mail->Subject  = "Contacto";
+$mail->WordWrap = 50;
+ 
+$mail->AddAddress("frander_mejia@outlook.com");           
+$mail->IsHTML(TRUE);
+
+$mensaje = $_POST['mensaje'];
+ 
+$mail->Body = $mensaje;
+
+$mail->Send();
+
+ 
+/*if(!$mail->Send()) {
+    echo "Error: " . $mail->ErrorInfo;
+} else {
+    echo "Mensaje enviado";
+}*/
+?>
+
+<script languaje="javascript">
+	alert("Mensaje enviado");
+	location.href = "index.html";
+</script>
