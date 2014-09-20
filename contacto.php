c2b88afa9c65e406d0beec638900774e68d3cf67
Frander <frander_mejia@outlook.com>
2014-09-20 11:53:51 -0600

    otra vez

--- contacto.php
+++ contacto.php
@@ -1,43 +1,75 @@
 <?php
-require 'libs/class.phpmailer.php';
+require 'libs/PHPMailerAutoload.php';
+/*
+$mail = new PHPMailer();
+$mail->isSendmail();
 
+$email = $_POST['email'];
+$nombre = $_POST['nombre'];
+
+$mail->setFrom($email, $nombre);
+
+$mail->addAddress('frander.mejia15@gmail.com', 'Frander Mejía');
+
+$mail->Subject = 'Contacto Portafolio';
+
+$mensaje = $_POST['mensaje'];
+
+$mail->AltBody = $mensaje;
+
+$mail->send()*/
 $mail = new PHPMailer();
  
 $mail->IsSMTP();
+
+$mail->SMTPDebug = 2;
+
 $mail->Host     = "ssl://smtp.gmail.com";
-$mail->Port     = 465;
+
+/*$mail->Port     = 465;*/
+
+$mail->Port = 587;
+
+$mail->SMTPSecure = 'tls';
+
 $mail->SMTPAuth = true;
  
 $mail->Username = "frander.mejia15@gmail.com";
+
 $mail->Password = "Guaymitas12";
 
 $email = $_POST['email'];
 $nombre = $_POST['nombre'];
+
+$mail->setFrom($email, $nombre);
+
+$mail->AddAddress("frander.mejia15@gmail.com"); 
  
-$mail->From     = $email
-$mail->FromName = $nombre
+/*$mail->From     = $email;
+$mail->FromName = $nombre;*/
  
 $mail->Subject  = "Contacto";
-$mail->WordWrap = 50;
- 
-$mail->AddAddress("frander.mejia15@gmail.com");           
-$mail->IsHTML(TRUE);
+
+/*$mail->WordWrap = 50;
+   
+$mail->IsHTML(TRUE);*/
 
 $mensaje = $_POST['mensaje'];
+
+/*$mail->AltBody = $mensaje;;*/
  
 $mail->Body = $mensaje;
 
-$mail->Send();
 
  
-/*if(!$mail->Send()) {
+if(!$mail->Send()) {
     echo "Error: " . $mail->ErrorInfo;
 } else {
     echo "Mensaje enviado";
-}*/
+}
 ?>
 
 <script languaje="javascript">
 	alert("Mensaje enviado");
-	location.href = "index.html";
+	location.href = "index.php";
 </script>
