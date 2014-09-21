765ba1b686d45fe77aba4a54d7ac89320981d6e2
Frander <frander_mejia@outlook.com>
2014-09-21 00:10:47 -0600

    responsive 4

--- index.php
+++ index.php
@@ -262,7 +262,7 @@
 			var imagePos = $(this).offset().top;
 
 			var topOfWindow = $(window).scrollTop();
-				if (imagePos < topOfWindow + 300) {
+				if (imagePos < topOfWindow + 200) {
 					$(this).addClass("slideRight");
 				}
 			});
