a59a1e1c08054cae8abacdcd614f8ae474c368a7
Frander <frander_mejia@outlook.com>
2014-11-12 08:35:07 -0600

    adición analitycs

--- index.php
+++ index.php
@@ -7,6 +7,12 @@
 		
 		<link rel="icon" type="image/png" href="img/favicon.png" />
 
+		<!-- METAS -->
+	     <meta name="description" content="Sitio de Desarrollador Web Nicaragua" />
+	     <meta name="keywords" content="Nicaragua Web Desarrollo Programacion" />
+	     <meta name="author" content="Frander Mejía" />
+	    <!-- /METAS -->
+
 		<link rel="stylesheet" type="text/css" href="css/animation.css">
 		<link rel="stylesheet" type="text/css" href="css/estilo.css">
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
@@ -310,6 +316,17 @@
 			    }
 		  	});
 		});
+
+	</script>
+
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-56678115-1', 'auto');
+	  ga('send', 'pageview');
 
 	</script>
 
